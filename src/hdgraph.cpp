06a9c8ce1e536be49156f00df360605ce06e219d
vezzo95 <vezzo.vz.95@gmail.com>
2020-01-08 17:19:48 +0100

    Update hdgraph.cpp
    Added Hasse reduction

--- src/hdgraph.cpp
+++ src/hdgraph.cpp
@@ -1,312 +1,323 @@
-#include "hdgraph.hpp"
+#ifndef HDGRAPH_HPP
+#define HDGRAPH_HPP
+
+#include <boost/graph/graph_utility.hpp>
+#include "globals.hpp"
+#include "rbgraph.hpp"
+
+//=============================================================================
+// Data structures
+
+/**
+  Scoped enumeration type whose underlying size is 1 byte, used for character
+  state.
+
+  State is paired with a character in the struct SignedCharacter.
+*/
+enum class State : bool {
+  lose,  ///< The paired character is lost
+  gain   ///< The paired character is gained
+};
+
+/**
+  @brief Struct used to represent a signed character
+
+  Each character c+ and c− is called a signed character.
+*/
+struct SignedCharacter {
+  std::string character{};    ///< Character name
+  State state = State::gain;  ///< Character state
+};
+
+//=============================================================================
+// Bundled properties
+
+/**
+  @brief Struct used to represent the properties of an edge (Hasse diagram)
+
+  The character c is gained in the edge (x, y) such that y is a child of x and
+  c has state 0 in x and state 1 in y.
+  In this case the edge (x, y) is labeled by c+.
+  Conversely, c is lost in the edge (x, y) if y is a child of x and character c
+  has state 1 in x and state 0 in y.
+  In the latter case the edge (x, y) is labeled by c−.
+  For each character c, we allow at most one edge labeled by c−.
+*/
+struct HDEdgeProperties {
+  std::list<SignedCharacter> signedcharacters{};  ///< List of SignedCharacters
+                                                  ///< that label the edge
+};
+
+/**
+  @brief Struct used to represent the properties of a vertex (Hasse diagram)
+
+  Given s a species, by C(s) we denote the set of characters of s.
+  Let GM be a maximal reducible graph.
+  Then the diagram P for GM is the Hasse diagram for the poset (Ps , ≤) of all
+  species of GM ordered by the relation ≤, where s1 ≤ s2 if C(s1) ⊆ C(s2).
+*/
+struct HDVertexProperties {
+  std::list<std::string> species{};  ///< List of species that label the vertex
+  std::list<std::string> characters{};  ///< List of characters of the species
+};
+
+/**
+  @brief Struct used to represent the properties of a Hasse diagram
+*/
+struct HDGraphProperties {
+  const RBGraph* g{};   ///< Original red-black graph
+  const RBGraph* gm{};  ///< Original maximal reducible graph
+  size_t num_v; ///< Number of vertices
+};
+
+//=============================================================================
+// Typedefs used for readabily
+
+// Graph
+
+/**
+  Hasse diagram
+*/
+typedef boost::adjacency_list<boost::setS,            // OutEdgeList
+                              boost::listS,            // VertexList
+                              boost::bidirectionalS,  // Directed
+                              HDVertexProperties,     // VertexProperties
+                              HDEdgeProperties,       // EdgeProperties
+                              HDGraphProperties       // GraphProperties
+                              >
+    HDGraph;
+
+// Descriptors
+
+/**
+  Edge of a Hasse diagram
+*/
+typedef boost::graph_traits<HDGraph>::edge_descriptor HDEdge;
+
+/**
+  Vertex of a Hasse diagram
+*/
+typedef boost::graph_traits<HDGraph>::vertex_descriptor HDVertex;
+
+// Iterators
+
+/**
+  Iterator of vertices (Hasse diagram)
+*/
+typedef boost::graph_traits<HDGraph>::vertex_iterator HDVertexIter;
+
+/**
+  Iterator of incoming edges (Hasse diagram)
+*/
+typedef boost::graph_traits<HDGraph>::in_edge_iterator HDInEdgeIter;
+
+/**
+  Iterator of outgoing edges (Hasse diagram)
+*/
+typedef boost::graph_traits<HDGraph>::out_edge_iterator HDOutEdgeIter;
+
+/**
+  Iterator (const) of a list of signed characters
+*/
+typedef std::list<SignedCharacter>::const_iterator SignedCharacterIter;
+
+// Size types
+
+/**
+  Size type of vertices (Hasse diagram)
+*/
+typedef boost::graph_traits<HDGraph>::vertices_size_type HDVertexSize;
+
+// Maps
+
+/**
+  Map of vertex indexes (Hasse diagram)
+*/
+typedef std::map<HDVertex, HDVertexSize> HDVertexIMap;
+
+/**
+  Associative property map of vertex indexes (Hasse diagram)
+*/
+typedef boost::associative_property_map<HDVertexIMap> HDVertexIAssocMap;
+
+typedef std::map<HDVertex, size_t> HDVertexIndexMap;
+typedef boost::associative_property_map<HDVertexIndexMap> HDVertexIndexAssocMap;
+
+//=============================================================================
+// Enum / Struct operator overloads
+
+/**
+  @brief Overloading of operator<< for State
+
+  @param[in] os Output stream
+  @param[in] s  State
+
+  @return Updated output stream
+*/
+inline std::ostream& operator<<(std::ostream& os, const State s) {
+  const bool sign = (s == State::lose);
+
+  return os << (sign ? "-" : "+");
+}
+
+/**
+  @brief Overloading of operator<< for SignedCharacter
+
+  @param[in] os Output stream
+  @param[in] sc SignedCharacter
+
+  @return Updated output stream
+*/
+inline std::ostream& operator<<(std::ostream& os, const SignedCharacter sc) {
+  return os << sc.character << sc.state;
+}
+
+/**
+  @brief Overloading of operator== for a pair of signed characters
+
+  @param[in] a SignedCharacter
+  @param[in] b SignedCharacter
+
+  @return True if a is equal to b
+*/
+inline bool operator==(const SignedCharacter& a, const SignedCharacter b) {
+  return (a.character == b.character && a.state == b.state);
+}
 
 //=============================================================================
 // Boost functions (overloading)
 
-// Hasse Diagram
+/**
+  @brief Add vertex with \e species and \e characters to \e hasse
 
+  @param[in]     species    List of species names
+  @param[in]     characters List of character names
+  @param[in,out] hasse      Hasse diagram graph
+
+  @return Vertex descriptor for the new vertex
+*/
 HDVertex add_vertex(const std::list<std::string>& species,
-                    const std::list<std::string>& characters, HDGraph& hasse) {
-  const auto v = boost::add_vertex(hasse);
-  hasse[v].species = species;
-  hasse[v].characters = characters;
+                    const std::list<std::string>& characters, HDGraph& hasse);
 
-  return v;
+/**
+  @brief Add vertex with \e species and \e characters to \e hasse
+
+  @param[in]     species    Species name
+  @param[in]     characters List of character names
+  @param[in,out] hasse      Hasse diagram graph
+
+  @return Vertex descriptor for the new vertex
+*/
+inline HDVertex add_vertex(const std::string& species,
+                           const std::list<std::string>& characters,
+                           HDGraph& hasse) {
+  return add_vertex(std::list<std::string>{species}, characters, hasse);
 }
 
+/**
+  @brief Add edge between \e u and \e v with a list of signed characters to
+         \e hasse
+
+  @param[in]     u                Source Vertex
+  @param[in]     v                Target Vertex
+  @param[in]     signedcharacters List of signed characters
+  @param[in,out] hasse            Hasse diagram graph
+
+  @return Edge descriptor for the new edge.
+          If the edge is already in the graph then a duplicate will not be
+          added and the bool flag will be false.
+          When the flag is false, the returned edge descriptor points to the
+          already existing edge
+*/
 std::pair<HDEdge, bool> add_edge(
     const HDVertex u, const HDVertex v,
-    const std::list<SignedCharacter>& signedcharacters, HDGraph& hasse) {
-  HDEdge e;
-  bool exists;
-  std::tie(e, exists) = boost::add_edge(u, v, hasse);
-  hasse[e].signedcharacters = signedcharacters;
-
-  return std::make_pair(e, exists);
-}
+    const std::list<SignedCharacter>& signedcharacters, HDGraph& hasse);
 
 //=============================================================================
 // General functions
 
-// Hasse Diagram
+/**
+  @brief Return a pointer to the original red-black graph of \e hasse
 
-std::ostream& operator<<(std::ostream& os, const HDGraph& hasse) {
-  HDVertexIter v, v_end;
-  std::tie(v, v_end) = vertices(hasse);
-  for (; v != v_end; ++v) {
-    os << "[ ";
+  @param[in] hasse Hasse diagram graph
 
-    for (const auto& i : hasse[*v].species) {
-      os << i << " ";
-    }
-
-    os << "( ";
-
-    for (const auto& i : hasse[*v].characters) {
-      os << i << " ";
-    }
-
-    os << ") ]:";
-
-    HDOutEdgeIter e, e_end;
-    std::tie(e, e_end) = out_edges(*v, hasse);
-    for (; e != e_end; ++e) {
-      const auto vt = target(*e, hasse);
-
-      os << " -";
-
-      auto j = hasse[*e].signedcharacters.cbegin();
-      for (; j != hasse[*e].signedcharacters.cend(); ++j) {
-        os << *j;
-
-        if (std::next(j) != hasse[*e].signedcharacters.cend()) os << ",";
-      }
-
-      os << "-> [ ";
-
-      for (const auto& i : hasse[vt].species) {
-        os << i << " ";
-      }
-
-      os << "( ";
-
-      for (const auto& i : hasse[vt].characters) {
-        os << i << " ";
-      }
-
-      os << ") ];";
-    }
-
-    if (std::next(v) != v_end) os << std::endl;
-  }
-
-  return os;
+  @return Pointer to the the original red-black graph of \e hasse
+*/
+inline const RBGraph* const orig_g(const HDGraph& hasse) {
+  return hasse[boost::graph_bundle].g;
 }
+
+/**
+  @brief Return a pointer to the original maximal reducible graph of \e hasse
+
+  @param[in] hasse Hasse diagram graph
+
+  @return Pointer to the the original maximal reducible graph of \e hasse
+*/
+inline const RBGraph* const orig_gm(const HDGraph& hasse) {
+  return hasse[boost::graph_bundle].gm;
+}
+
+/**
+  @brief Overloading of operator<< for HDGraph
+
+  @param[in] os    Output stream
+  @param[in] hasse Hasse diagram graph
+
+  @return Updated output stream
+*/
+std::ostream& operator<<(std::ostream& os, const HDGraph& hasse);
 
 //=============================================================================
 // Algorithm functions
 
+/**
+  @brief Returns True if \e a is included in \e b
+
+  @param[in] a List of character names (strings)
+  @param[in] b List of character names (strings)
+
+  @return True if \e a is included in \e b, False otherwise
+*/
 bool is_included(const std::list<std::string>& a,
-                 const std::list<std::string>& b) {
-  for (const auto& a_str : a) {
-    if (std::find(b.cbegin(), b.cend(), a_str) == b.cend())
-      // exit the function at the first string of a not present in b
-      return false;
-  }
+                 const std::list<std::string>& b);
 
-  return true;
+/**
+  @brief Build the Hasse diagram of \e gm
+
+  Let GM be a maximal reducible graph.
+  Then the diagram P for GM is the Hasse diagram for the poset (Ps, ≤) of all
+  species of GM ordered by the relation ≤, where s1 ≤ s2 if C(s1) ⊆ C(s2).
+  Given (Ps, ≤) the poset of all species of a red-black graph, we
+  consider the representation of the poset (Ps, ≤) by its Hasse diagram,
+  represented by a directed acyclic graph P.
+  More precisely, two species s1 and s2 are connected by the arc (s1, s2) if
+  s1 < s2 and there does not exist a species s3 such that s1 < s3 < s2.
+
+  @param[out] hasse Hasse diagram graph
+  @param[in]  g     Red-black graph
+  @param[in]  gm    Maximal reducible red-black graph
+*/
+void hasse_diagram(HDGraph& hasse, const RBGraph& g, const RBGraph& gm);
+
+/**
+  @brief Removes active species from an hasse diagram
+
+  A reduced HDGraph is the one with active species eliminated.
+  A specie is active if it has red edges incident to it.
+
+  @param [in] hasse Hasse diagram graph
+  @param [in] g     Red-black graph
+
+  @return Reduced Hasse diagram graph
+*/
+void reduce_diagram(HDGraph& hasse, const RBGraph& gm);
+
+
+void transitive_reduction(HDGraph& hasse); 
+void remove_vertex(HDVertex& v, HDGraph& p);
+inline size_t num_vertices(const HDGraph& hasse) {
+  return hasse[boost::graph_bundle].num_v;
 }
 
-void hasse_diagram(HDGraph& hasse, const RBGraph& g, const RBGraph& gm) {
-  std::vector<std::list<RBVertex>> vec_adj_char(num_species(gm));
-  std::map<RBVertex, std::list<RBVertex>> adj_char;
-
-  // how vec_adj_char is going to be structured:
-  // vec_adj_char[index] => < S, List of characters adjacent to S >
-
-  // how adj_char is going to be structured:
-  // adj_char[S] => < List of characters adjacent to S >
-
-  // vec_adj_char is used to sort the lists by number of elements, this is why
-  // we store the list of adjacent characters to S. While we store S to be able
-  // to access adj_char[S] in costant time, instead of iterating on vec_adj_char
-  // to find the correct list
-
-  auto compare_names = [](const std::string& a, const std::string& b) {
-    size_t a_index, b_index;
-    std::stringstream ss;
-
-    ss.str(a.substr(1));
-    ss >> a_index;
-
-    ss.clear();
-
-    ss.str(b.substr(1));
-    ss >> b_index;
-
-    return a_index < b_index;
-  };
-
-  // initialize vec_adj_char and adj_char for each species in the graph
-  RBVertexIter v, v_end;
-  std::tie(v, v_end) = vertices(gm);
-  for (size_t index = 0; v != v_end; ++v) {
-    if (!is_species(*v, gm)) continue;
-    // for each species vertex
-
-    // vec_adj_char[index]'s first element is the species vertex
-    vec_adj_char[index].push_back(*v);
-
-    // build v's set of adjacent characters
-    RBOutEdgeIter e, e_end;
-    std::tie(e, e_end) = out_edges(*v, gm);
-    for (; e != e_end; ++e) {
-      // ignore active characters
-      if (is_red(*e, gm)) continue;
-
-      // vt = one of the characters adjacent to *v
-      const auto vt = target(*e, gm);
-
-      // vec_adj_char[index]'s other elements are the characters adjacent to S
-      vec_adj_char[index].push_back(vt);
-      // adj_char[S]'s elements are the characters adjacent to S
-      adj_char[*v].push_back(vt);
-    }
-
-    // if the species *v would have 0 characters, ignore it
-    if (vec_adj_char[index].size() == 1) {
-      vec_adj_char[index].clear();
-
-      continue;
-    }
-
-    index++;
-  }
-
-  auto compare_size = [](const std::list<RBVertex>& a,
-                         const std::list<RBVertex>& b) {
-    return a.size() < b.size();
-  };
-
-  // sort vec_adj_char by size in ascending order
-  std::sort(vec_adj_char.begin(), vec_adj_char.end(), compare_size);
-
-  bool first_iteration = true;
-  for (const auto& set : vec_adj_char) {
-    // for each set of characters
-    if (set.empty()) continue;
-
-    // v = species of gm
-    const auto v = set.front();
-
-    // fill the list of characters names of v
-    std::list<std::string> lcv{};
-    for (const auto& cv : adj_char[v]) {
-      lcv.push_back(gm[cv].name);
-    }
-
-    lcv.sort(compare_names);
-
-    if (first_iteration) {
-      // first iteration of the loop:
-      // add v to the Hasse diagram, and being the first vertex of the graph
-      // there's no need to do any work
-      add_vertex(gm[v].name, lcv, hasse);
-
-      first_iteration = false;
-
-      continue;
-    }
-
-    // new_edges will contain the list of edges that may be added to the Hasse
-    // diagram: HDVertex is the source, std::string is the edge label
-    std::list<std::pair<HDVertex, std::string>> new_edges;
-
-    // check if there is a vertex with the same characters as v or if v needs
-    // to be added to the Hasse diagram
-    HDVertexIter hdv, hdv_end;
-    std::tie(hdv, hdv_end) = vertices(hasse);
-    for (; hdv != hdv_end; ++hdv) {
-      // for each vertex in hasse
-
-      if (lcv == hasse[*hdv].characters) {
-        // v and hdv have the same characters
-
-        // add v to the list of species in hdv
-        hasse[*hdv].species.push_back(gm[v].name);
-
-        break;
-      }
-
-      const std::list<std::string> lhdv = hasse[*hdv].characters;
-
-      // TODO: check if edge building is done right
-
-      // TODO: is_included might be superfluous
-      // initialize new_edges if lhdv is a subset of lcv, with the structure:
-      // *hdv -*ci-> v
-      if (is_included(lhdv, lcv)) {
-        // hdv is included in v
-        for (const auto& ci : lcv) {
-          // for each character in hdv
-          const auto in = std::find(lhdv.cbegin(), lhdv.cend(), ci);
-
-          if (in == lhdv.end()) {
-            // character is not present in lhdv
-            new_edges.push_back(std::make_pair(*hdv, ci));
-          }
-        }
-      }
-
-      // on the last iteration of the cycle, after having collected all the
-      // pairs in new_edges, add vertex and edges to the Hasse diagram
-      if (std::next(hdv) == hdv_end) {
-        // last iteration on the characters in the list has been performed
-
-        // build a vertex for v and add it to the Hasse diagram
-        const auto u = add_vertex(gm[v].name, lcv, hasse);
-
-        // build in_edges for the vertex and add them to the Hasse diagram
-        for (const auto& ei : new_edges) {
-          // for each new edge to add to the Hasse diagram
-          HDEdge edge;
-          std::tie(edge, std::ignore) = add_edge(ei.first, u, hasse);
-          hasse[edge].signedcharacters.push_back({ei.second, State::gain});
-        }
-
-        break;
-      }
-    }
-  }
-
-  // Store the graph pointer into the Hasse diagram's graph properties
-  hasse[boost::graph_bundle].g = &g;
-
-  // Store the maximal reducible graph pointer into the Hasse diagram's graph
-  // properties
-  hasse[boost::graph_bundle].gm = &gm;
-
-  // transitive reduction of the Hasse diagram
-  HDVertexIter u, u_end;
-  std::tie(u, u_end) = vertices(hasse);
-  for (; u != u_end; ++u) {
-    if (in_degree(*u, hasse) == 0 || out_degree(*u, hasse) == 0) continue;
-    // for each internal vertex in hasse
-
-    HDInEdgeIter ie, ie_end;
-    std::tie(ie, ie_end) = in_edges(*u, hasse);
-    for (; ie != ie_end; ++ie) {
-      // for each in-edge
-      HDOutEdgeIter oe, oe_end;
-      std::tie(oe, oe_end) = out_edges(*u, hasse);
-      for (; oe != oe_end; ++oe) {
-        // for each out-edge
-        // source -> u -> target
-        HDEdge e;
-        bool exists;
-        std::tie(e, exists) =
-            edge(source(*ie, hasse), target(*oe, hasse), hasse);
-
-        if (!exists)
-          // no transitivity between source and target
-          continue;
-
-        // remove source -> target, which breaks the no-transitivty rule in
-        // the Hasse diagram we need, because a path between source and target
-        // already exists in the form: source -> u -> target
-        remove_edge(e, hasse);
-      }
-    }
-  }
-
-  // sort species names in each vertex
-  std::tie(u, u_end) = vertices(hasse);
-  for (; u != u_end; ++u) {
-    auto species = hasse[*u].species;
-
-    species.sort(compare_names);
-
-    hasse[*u].species.clear();
-    hasse[*u].species.splice(hasse[*u].species.cend(), species);
-  }
-}
+#endif  // HDGRAPH_HPP
